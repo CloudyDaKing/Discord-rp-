e53c890a1afcb08423867d184598e150b231fb3a
CloudyDaKing <87387633+CloudyDaKing@users.noreply.github.com>
2022-11-05 20:04:39 +0000

    added utils file

--- main.py
+++ main.py
@@ -1,12 +1,4 @@
-"""
-This is the main loop file for our AutoTube Bot!
-
-Quick notes!
-- Currently it's set to try and post a video then sleep for a day.
-- You can change the size of the video currently it's set to post shorts.
-    * Do this by adding a parameter of scale to the image_save function.
-    * scale=(width,height)
-"""
+ 
 
 from datetime import date
 import time
